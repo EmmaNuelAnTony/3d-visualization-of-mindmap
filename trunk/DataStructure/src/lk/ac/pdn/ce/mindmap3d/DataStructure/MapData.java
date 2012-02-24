aaa934f79ad4e097df4a9144c2cd3392f6c02ecb
mail2ppa@gmail.com <mail2ppa@gmail.com@369e3057-5047-95b0-ec4e-6e433009870d>
2012-02-24 08:50:03 +0000

    Complete DataSructure + XML read Write facility

--- trunk/DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/MapData.java
+++ trunk/DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/MapData.java
@@ -1,7 +1,10 @@
 package lk.ac.pdn.ce.mindmap3d.DataStructure;
 
+import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
+
+import org.xml.sax.SAXException;
 
 /**
  * represent a mindmap. starting from root node
@@ -10,41 +13,62 @@ import javax.xml.transform.TransformerException;
  */
 public class MapData {
 	
-	MMElement root;
+	private MMElement root;
 	
+	/**
+	 * Initialize data Structure
+	 * @param mapName name for the root element
+	 */
 	public MapData(String mapName){
 		root=new MMElement();
 		root.setName(mapName);
 	}
 	
+	/**
+	 * @return root element of the MindMap
+	 */
 	public MMElement getRoot(){
 		return root;
 	}
 	
+	/**
+	 * introduce new Element to mindmap 
+	 * @param nElement new element
+	 * @param parent parent element
+	 */
 	public synchronized void addElement(MMElement nElement,MMElement parent){
 		parent.addChild(nElement);
 		nElement.setParent(parent);
 	}
 	
+	/**
+	 * To remove existing element from the map
+	 * @param cElement
+	 */
 	public synchronized void removeElement(MMElement cElement){
 		//remove connections
 		cElement.getParent().removeChild(cElement);
 		cElement.setParent(null);
 	}
 	
-	public synchronized void loadXML(String fileName){
-		XMLFile.retriveXML(fileName, root);
+	/**
+	 * To load new mind map from file.Existing map will be deleted
+	 * @param fileName
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException 
+	 */
+	public synchronized void loadXML(String fileName) throws ParserConfigurationException, SAXException, IOException{
+		XMLFile.retriveXML(fileName, this);
 	}
 	
-	public synchronized void saveXML(String fileName){
-		try {
-			XMLFile.saveXML(fileName, root);
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	/**
+	 * Save existing mind map into a file 
+	 * @param fileName
+	 * @throws TransformerException 
+	 * @throws ParserConfigurationException 
+	 */
+	public synchronized void saveXML(String fileName) throws ParserConfigurationException, TransformerException{
+		XMLFile.saveXML(fileName, root);
 	}
 }
--- trunk/DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/test1.java
+++ trunk/DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/test1.java
@@ -2,16 +2,72 @@ package lk.ac.pdn.ce.mindmap3d.DataStructure;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+/**
+ * Unit test
+ * @author QBTNMEV
+ *
+ */
 public class test1 {
 
 	@Test
-	public void saveTest() {
-		System.out.println("**********************************");
+	public void Test() {
 		MapData m1=new MapData("mind map1 save test ");
-	
-		m1.saveXML("test.xml");
+		try {
+			m1.loadXML("test.xml");
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SAXException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			m1.saveXML("test1.xml");
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TransformerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		//compare documents
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setCoalescing(true);
+		dbf.setIgnoringElementContentWhitespace(true);
+		dbf.setIgnoringComments(true);
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc1 = db.parse(new File("test.xml"));
+			doc1.normalizeDocument();
+			Document doc2 = db.parse(new File("test1.xml"));
+			doc2.normalizeDocument();
+			assertTrue("Error",doc1.isEqualNode(doc2));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+		
 	}
 
 }
