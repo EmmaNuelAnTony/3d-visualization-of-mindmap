b8389f3b4db4f8b5dcc963c0628500816552d4f0
mail2ppa <mail2ppa@gmail.com>
2012-02-24 05:39:51 +0000

    Add javadoc

--- DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/MMElement.java
+++ DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/MMElement.java
@@ -1,6 +1,14 @@
 package lk.ac.pdn.ce.mindmap3d.DataStructure;
 
+/**
+ * Represent single element in mindmap
+ * @author Pasindu Pavithra Ariyarathna
+ *
+ */
 public class MMElement {
 	private String id;
 	private String name;
+	private Position position;
+	private MMElement parent;
+	private MMElement[] children;
 }
