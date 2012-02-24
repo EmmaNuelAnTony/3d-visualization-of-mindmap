7e2d386d89077541fe71a199becaef39948e38e1
mail2ppa <mail2ppa@gmail.com>
2012-02-24 07:30:01 +0000

    XML Save functionality

---
+++ DataStructure/src/lk/ac/pdn/ce/mindmap3d/DataStructure/Position.java
@@ -0,0 +1,43 @@
+package lk.ac.pdn.ce.mindmap3d.DataStructure;
+
+/**
+ * Position in 3d SPACE
+ * @author Pasindu Pavithra Ariyarathna
+ *
+ */
+public class Position {
+	private double X;
+	private double Y;
+	private double Z;
+	
+	public Position(){
+		super();
+	}
+	
+	public Position(double x, double y, double z) {
+		super();
+		X = x;
+		Y = y;
+		Z = z;
+	}
+	
+	public double getX() {
+		return X;
+	}
+	public void setX(double x) {
+		X = x;
+	}
+	public double getY() {
+		return Y;
+	}
+	public void setY(double y) {
+		Y = y;
+	}
+	public double getZ() {
+		return Z;
+	}
+	public void setZ(double z) {
+		Z = z;
+	}
+	
+}
