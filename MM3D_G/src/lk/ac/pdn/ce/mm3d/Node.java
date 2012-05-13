3c1871f6468e0f08f5634c731ba9168bc271ff88
mail2ppa <mail2ppa@gmail.com>
2012-05-13 03:47:54 +0000

    Add Color scheme

--- MM3D_G/src/lk/ac/pdn/ce/mm3d/Node.java
+++ MM3D_G/src/lk/ac/pdn/ce/mm3d/Node.java
@@ -6,36 +6,42 @@ import com.threed.jpct.Object3D;
 import com.threed.jpct.Primitives;
 import com.threed.jpct.RGBColor;
 
-public class Node extends Object3D{
-	
-	
-	private Node parent;//parent node of the object
-	private Link parentLink;//link for object to it's parent
-	private int level;//map level
-	private MMElement mmElement;//datastructure element
-	
-	
-	public Node(RGBColor color){			// no parent means root node
+public class Node extends Object3D {
+
+	private Node parent;// parent node of the object
+	private Link parentLink;// link for object to it's parent
+	private int level;// map level
+	private MMElement mmElement;// datastructure element
+
+	public Node(RGBColor color) { // no parent means root node
 		super(Primitives.getSphere(5));
 		this.setAdditionalColor(color);
-		level=0;
+		level = 0;
 	}
-	
-	public Node(Node p,RGBColor color){
+
+	public Node(Node p, RGBColor color) {
 		super(Primitives.getSphere(4));
 		this.setAdditionalColor(color);
-		parent=p;
-		level=parent.getLevel();
+		parent = p;
+		level = parent.getLevel();
 	}
-	
-	public Node(Node p){
+
+	public Node(Node p) {
 		super(Primitives.getSphere(4));
-		this.setAdditionalColor(new RGBColor((int) (Math.random()*255),(int) (Math.random()*255),(int) (Math.random()*255)));
-		parent=p;
-		level=parent.getLevel()+1;
+		this.setAdditionalColor(new RGBColor((int) (Math.random()*255),(int)
+		 (Math.random()*255),(int) (Math.random()*255)));
+		parent = p;
+		level = parent.getLevel() + 1;
+//		RGBColor c1 = null;
+//		switch(level){
+//		case 1:c1 = new RGBColor(255, 0, 0);break;
+//		case 2:c1 = new RGBColor(0, 255, 0);break;
+//		case 3:c1 = new RGBColor(0, 0, 255);break;
+//		default:c1 = new RGBColor(0, 0, 0);
+//		}
+//		this.setAdditionalColor(c1);
 	}
-	
-	
+
 	public int getLevel() {
 		return level;
 	}
@@ -43,11 +49,10 @@ public class Node extends Object3D{
 	public Node getParent() {
 		return parent;
 	}
-	
+
 	public void setParent(Node parent) {
-		this.parent=parent;
+		this.parent = parent;
 	}
-	
 
 	public void setParentLink(Link parentLink) {
 		this.parentLink = parentLink;
@@ -57,8 +62,6 @@ public class Node extends Object3D{
 		return parentLink;
 	}
 
-	
-
 	public MMElement getMmElement() {
 		return mmElement;
 	}
@@ -67,17 +70,12 @@ public class Node extends Object3D{
 		this.mmElement = mmElement;
 	}
 
-	public void buildParentLink(){
-		if (parent==null){
+	public void buildParentLink() {
+		if (parent == null) {
 			return;
 		}
-		parentLink=new Link(mmElement);
+		parentLink = new Link(mmElement);
 
 	}
-	
-	
-	
 
-	
-	
 }
