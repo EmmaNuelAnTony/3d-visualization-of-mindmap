b1fba4e123e8e163b1b2fe74718b3ab8d80d3e34
akalankapagoda@gmail.com <akalankapagoda@gmail.com@369e3057-5047-95b0-ec4e-6e433009870d>
2012-06-10 16:05:03 +0000

    Add file explorer for import_xml and export_xml.

--- trunk/MM3D_G/gen/lk/ac/pdn/ce/mm3d/R.java
+++ trunk/MM3D_G/gen/lk/ac/pdn/ce/mm3d/R.java
@@ -10,32 +10,61 @@ package lk.ac.pdn.ce.mm3d;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int gray=0x7f050000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int file=0x7f020000;
+        public static final int folder=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
     }
     public static final class id {
-        public static final int add_node=0x7f060006;
-        public static final int buttonAdd=0x7f060005;
-        public static final int export_xml=0x7f060009;
-        public static final int import_xml=0x7f060008;
-        public static final int remove_node=0x7f060007;
-        public static final int textInfo=0x7f060004;
-        public static final int textName=0x7f060003;
-        public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060001;
-        public static final int textView3=0x7f060002;
+        public static final int add_node=0x7f070013;
+        public static final int buttonAdd=0x7f070012;
+        public static final int export_xml=0x7f070016;
+        public static final int fdButtonCancel=0x7f070008;
+        public static final int fdButtonCreate=0x7f070009;
+        public static final int fdButtonNew=0x7f070003;
+        public static final int fdButtonSelect=0x7f070004;
+        public static final int fdEditTextFile=0x7f070007;
+        public static final int fdLinearLayoutCreate=0x7f070005;
+        public static final int fdLinearLayoutList=0x7f070001;
+        public static final int fdLinearLayoutSelect=0x7f070002;
+        public static final int fdrowimage=0x7f07000b;
+        public static final int fdrowtext=0x7f07000c;
+        public static final int import_xml=0x7f070015;
+        public static final int path=0x7f07000a;
+        public static final int relativeLayout01=0x7f070000;
+        public static final int remove_node=0x7f070014;
+        public static final int textInfo=0x7f070011;
+        public static final int textName=0x7f070010;
+        public static final int textView1=0x7f07000d;
+        public static final int textView2=0x7f07000e;
+        public static final int textView3=0x7f07000f;
+        public static final int textViewFilename=0x7f070006;
     }
     public static final class layout {
-        public static final int layout1=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int file_dialog_main=0x7f030000;
+        public static final int file_dialog_row=0x7f030001;
+        public static final int layout1=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int context_menu=0x7f050000;
-        public static final int main_menu=0x7f050001;
+        public static final int context_menu=0x7f060000;
+        public static final int main_menu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int cancel=0x7f040007;
+        public static final int cant_read_folder=0x7f040003;
+        public static final int create=0x7f040008;
+        public static final int err=0x7f04000a;
+        public static final int file_name=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int location=0x7f040002;
+        public static final int nnew=0x7f040004;
+        public static final int no_data=0x7f040009;
+        public static final int select=0x7f040005;
     }
 }
